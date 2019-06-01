7d29fec7417fae67963883b903268e014284ac1d
Krishna Keshan <krishnakeshan18@gmail.com>
2019-06-02 00:32:08 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
-document.querySelector('h1').onclick = function() {
-  alert("Aye!");
+var myHeading = document.querySelector('h1')
+var myButton = document.querySelector('button')
+
+function setUserName() {
+  var userName = prompt("Please enter your username: ");
+  localStorage.setItem("name", userName);
+  myHeading.textContent = "Mozilla is cute, " + userName;
 }
